9421e9c2b0a0039b72c9d937930477449f03dc45
fcjcbk <1097086201@qq.com>
2023-12-29 00:04:27 +0800

    add select course, todo: do not fresh the course list in man.vue

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router/index.ts'
 import UndrawUi from 'undraw-ui'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import 'undraw-ui/dist/style.css'
 import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
