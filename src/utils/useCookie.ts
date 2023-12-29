9cf0ce9403938546aaaf2895478183b794096d84
fcjcbk <1097086201@qq.com>
2023-12-29 20:57:48 +0800

    change cookie expire time

--- src/utils/useCookie.ts
+++ src/utils/useCookie.ts
@@ -1,24 +1,11 @@
 export function setCookie(
     name: string,
     value: string | number, // 包含账号和密码
-    expires: number = 1, // 以小时为单位且最小为1
+    expires: number = 15, // 以小时为单位且最小为1
 ){
-    let Days = expires;
+    let Minutes = expires; // Change variable name to Minutes
     let exp = new Date();
-    exp.setTime(exp.getTime() + Days * 60 * 60 * 1000);
+    exp.setTime(exp.getTime() + Minutes * 60 * 1000); // Change calculation to minutes
     document.cookie = name + "=" + encodeURIComponent(value) + `expires=${exp.toUTCString()}`
     // document.cookie = encodeURIComponent(value) + `.${exp.toUTCString()}`
-}   
-
-
-export function getCookie(name: string){
-    const reg = new RegExp("(^| )" + encodeURIComponent(name) + "expires=");
-    const match = document.cookie.match(reg)
-    // console.log(match)
-    return match ? decodeURIComponent(match[2]) : ""
-    
-}
-
-export function removeCookie(name: string){
-    setCookie(name, "", -1) //相当于直接过期
 }
